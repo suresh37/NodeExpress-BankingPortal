690e20258da1adba62db1c52ac1162a426d231db
suresh37 <sureshkarthick37@gmail.com>
2019-01-09 00:03:03 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('index', {
-        title: "Express-Banking Portal"
+        title: "Index"
     })
 })
 
