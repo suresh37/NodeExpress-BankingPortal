8b4b523aaa68fbf79d70dcbef6e4ef8e17b6cc3c
suresh37 <sureshkarthick37@gmail.com>
2019-01-09 00:24:30 +0530

    completed the module

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('index', {
