497da291f08b5fd95fa38aad27a909c7f9b166a1
suresh37 <sureshkarthick37@gmail.com>
2019-01-08 23:27:54 +0530

    Completed the module

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "Express-Banking Portal"
+    })
+})
+
+app.listen(3000, (req, res) => {
+    console.log('app: listening on 3000')
+})
